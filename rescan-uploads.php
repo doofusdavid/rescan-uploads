53eb199b10053339616aa446d555e0e77951d679
David Edwards <david.edwards@colostate.edu>
2016-10-11 15:06:46 -0600

    Initial Commit

---
+++ rescan-uploads.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Rescan Uploads
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Rescans your upload (or other) folders to add files to your media library.
+ * Author:          David Edwards
+ * Author URI:      http://david.edwards.cloud
+ * Text Domain:     rescan-uploads
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Rescan_Uploads
+ */
